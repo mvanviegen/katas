844d33b79746e9bc517a8f51dbf57b853140f8b7
mikevanviegen <mike.van.viegen@rabobank.nl>
2021-03-19 10:51:48 +0100

    easier readings

--- kotlin/code-katas/parity-outlier/src/main/kotlin/ParityOutlier.kt
+++ kotlin/code-katas/parity-outlier/src/main/kotlin/ParityOutlier.kt
@@ -2,16 +2,24 @@ internal class ParityOutlier {
     infix fun find(numbers: List<Int>): Int {
         // check whether given list is (mostly) even or odd
 
-        if ((numbers[0] % 2 == 0 && numbers[1] % 2 == 0) || (numbers[0] % 2 == 0 && numbers[2] % 2 == 0)) {
+        if ((numbers[0].isEven() && numbers[1].isEven()) || (numbers[0].isEven() && numbers[2].isEven())) {
             // even array; should filter out non-even number
             return numbers.filter { it % 2 != 0 }.get(0)
         }
 
-        if ((numbers[0] % 2 != 0 && numbers[1] % 2 != 0) || (numbers[0] % 2 != 0 && numbers[2] % 2 != 0)) {
+        if ((numbers[0].isOdd() && numbers[1].isOdd()) || (numbers[0].isOdd() && numbers[2].isOdd())) {
             // even array; should filter out non-even number
             return numbers.filter { it % 2 == 0 }.get(0)
         }
 
         return numbers.get(0)
     }
+}
+
+private fun Int.isEven(): Boolean {
+    return this % 2 == 0
+}
+
+private fun Int.isOdd(): Boolean {
+    return this % 2 != 0
 }
