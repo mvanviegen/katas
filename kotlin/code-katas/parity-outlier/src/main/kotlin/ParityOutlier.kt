bd8d216c9d908346a8988c265cd85f48ce5d9039
mikevanviegen <mike.van.viegen@rabobank.nl>
2021-03-19 10:48:43 +0100

    beautifull production code first...make it work

--- kotlin/code-katas/parity-outlier/src/main/kotlin/ParityOutlier.kt
+++ kotlin/code-katas/parity-outlier/src/main/kotlin/ParityOutlier.kt
@@ -1,5 +1,17 @@
 internal class ParityOutlier {
-    fun findOutlier(numbers: List<Int>): Int {
-        return numbers.filter { it % 2 == 0 }.get(0)
+    infix fun find(numbers: List<Int>): Int {
+        // check whether given list is (mostly) even or odd
+
+        if ((numbers[0] % 2 == 0 && numbers[1] % 2 == 0) || (numbers[0] % 2 == 0 && numbers[2] % 2 == 0)) {
+            // even array; should filter out non-even number
+            return numbers.filter { it % 2 != 0 }.get(0)
+        }
+
+        if ((numbers[0] % 2 != 0 && numbers[1] % 2 != 0) || (numbers[0] % 2 != 0 && numbers[2] % 2 != 0)) {
+            // even array; should filter out non-even number
+            return numbers.filter { it % 2 == 0 }.get(0)
+        }
+
+        return numbers.get(0)
     }
 }
--- kotlin/code-katas/parity-outlier/src/test/kotlin/ParityOutlierTest.kt
+++ kotlin/code-katas/parity-outlier/src/test/kotlin/ParityOutlierTest.kt
@@ -1,12 +1,28 @@
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class ParityOutlierTest {
+    private lateinit var parityOutlier: ParityOutlier
+
+    @BeforeEach
+    internal fun setUp() {
+        parityOutlier = ParityOutlier()
+    }
+
     @Test
     internal fun `should return the even number given list of odd numbers`() {
-        val result = ParityOutlier().findOutlier(listOf(1, 2, 3))
+        val result = parityOutlier find listOf(1, 2, 3)
 
         assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    internal fun `should return the odd number given list of even numbers`() {
+        val numbers = listOf(206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781)
+        val result = parityOutlier find numbers
+
+        assertThat(result).isEqualTo(206847684)
     }
 }
