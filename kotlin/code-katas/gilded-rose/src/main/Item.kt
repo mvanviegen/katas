b4cc4b852c22b613244de199e84290bc27b8a5a2
mikevanviegen <mike.van.viegen@rabobank.nl>
2022-02-02 16:02:50 +0100

    added base gilded rose

---
+++ kotlin/code-katas/gilded-rose/src/main/Item.kt
@@ -0,0 +1,6 @@
+
+open class Item(var name: String, var sellIn: Int, var quality: Int) {
+    override fun toString(): String {
+        return this.name + ", " + this.sellIn + ", " + this.quality
+    }
+}
