7304d0cca7a96584c8803d720393f418b88561bb
mikevanviegen <mike.van.viegen@rabobank.nl>
2022-02-05 15:49:50 +0100

    initial parsing of fizz n buzz

--- kotlin/code-katas/fizzbuzz/src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ kotlin/code-katas/fizzbuzz/src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -1,26 +1,11 @@
 package fizzbuzz
 
 class FizzBuzz {
-    fun translate(input: List<Int>): String {
-        val result = input.map { number ->
-
-            if( number % 15 == 0 ) {
-                "FizzBuzz"
-            } else if( number % 5 == 0 ) {
-                "Buzz"
-            } else if( number % 3 == 0 ) {
-                "Fizz"
-            } else {
-                number.toString()
-            }
-
-        }.joinToString(", ")
-
-        println(result)
-        return result
+    fun parse(input: Int): String {
+        if(input == 5) return "Buzz"
+        return "Fizz"
     }
 }
 
-fun main() {
-    FizzBuzz().translate(listOf(1, 2, 3, 4))
+fun main() : Unit {
 }
--- kotlin/code-katas/fizzbuzz/src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ kotlin/code-katas/fizzbuzz/src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -5,48 +5,24 @@ import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 internal class FizzBuzzTest {
+    private val fizzBuzz = FizzBuzz()
+
     @Test
-    internal fun `should return the number when number is not divisible by three`() {
-        val input = listOf(1, 2)
-
-        val result = FizzBuzz().translate(input)
-
-        assertThat(result).isEqualTo("1, 2")
+    internal fun `should print Fizz given multiples of three`() {
+        assertThat(fizzBuzz.parse(3)).isEqualTo("Fizz")
     }
 
     @Test
-    internal fun `should return the word fizz when number is divisible by three`() {
-        val input = listOf(1, 2, 3)
+    internal fun `should print Buzz given multiples of five`() {
+        val result = fizzBuzz.parse(5)
 
-        val result = FizzBuzz().translate(input)
-
-        assertThat(result).isEqualTo("1, 2, Fizz")
+        assertThat(result).isEqualTo("Buzz")
     }
 
     @Test
-    internal fun `should return the word buzz when given number is divisible by five`() {
-        val input = listOf(1, 2, 5)
+    internal fun `should not parse given no three or five multiplicatives`() {
+        val result = fizzBuzz.parse(1)
 
-        val result = FizzBuzz().translate(input)
-
-        assertThat(result).isEqualTo("1, 2, Buzz")
-    }
-
-    @Test
-    internal fun `should return the word fizzbuzz when given number is divisible by fifteen`() {
-        val input = listOf(1, 2, 15)
-
-        val result = FizzBuzz().translate(input)
-
-        assertThat(result).isEqualTo("1, 2, FizzBuzz")
-    }
-
-    @Test
-    internal fun `should translate the correct words on given numbers`() {
-        val input = (1..100).toList()
-
-        val result = FizzBuzz().translate(input)
-
-        assertThat(result).isEqualTo("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, Buzz")
+        assertThat(result).isEqualTo("1")
     }
 }
