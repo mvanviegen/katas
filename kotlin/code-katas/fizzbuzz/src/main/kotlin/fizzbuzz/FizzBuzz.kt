d2b45a129467a955dfd1f26db9c9c80fd66f8720
mikevanviegen <mike.van.viegen@rabobank.nl>
2022-02-05 17:11:31 +0100

    refactor when to reference method

--- kotlin/code-katas/fizzbuzz/src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ kotlin/code-katas/fizzbuzz/src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -2,14 +2,17 @@ package fizzbuzz
 
 class FizzBuzz {
     infix fun parse(input: List<Int>) =
-        input.map {
-            when {
-                it % 15 == 0 -> "FizzBuzz"
-                it % 3 == 0 -> "Fizz"
-                it % 5 == 0 -> "Buzz"
-                else -> it.toString()
-            }
-        }.joinToString(", ")
+        input
+            .map(::parseIndividual)
+            .joinToString("\n")
+
+    private fun parseIndividual(value: Int) =
+        when {
+            value % 15 == 0 -> "FizzBuzz"
+            value % 3 == 0 -> "Fizz"
+            value % 5 == 0 -> "Buzz"
+            else -> value.toString()
+        }
 }
 
 fun main(): Unit {
--- kotlin/code-katas/fizzbuzz/src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ kotlin/code-katas/fizzbuzz/src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -11,28 +11,34 @@ internal class FizzBuzzTest {
     internal fun `should print Fizz given multiples of three`() {
         val result = fizzBuzz parse listOf(3, 9)
 
-        println(result)
-        assertThat(result).isEqualTo("Fizz, Fizz")
+        assertThat(result).isEqualTo("Fizz\nFizz")
     }
 
     @Test
     internal fun `should print Buzz given multiples of five`() {
         val result = fizzBuzz parse listOf(5, 10)
 
-        assertThat(result).isEqualTo("Buzz, Buzz")
+        assertThat(result).isEqualTo("Buzz\nBuzz")
     }
 
     @Test
     internal fun `should not parse given no three or five multiplicatives`() {
         val result = fizzBuzz parse listOf(1, 2, 4)
 
-        assertThat(result).isEqualTo("1, 2, 4")
+        assertThat(result).isEqualTo("1\n2\n4")
     }
 
     @Test
     internal fun `should print FizzBuzz given multiples of fifteen`() {
         val result = fizzBuzz parse listOf(15, 30)
 
-        assertThat(result).isEqualTo("FizzBuzz, FizzBuzz")
+        assertThat(result).isEqualTo("FizzBuzz\nFizzBuzz")
+    }
+
+    @Test
+    internal fun `should parse correctly given 1 to 100`() {
+        val result = fizzBuzz parse (1..100).toList()
+
+        assertThat(result).isEqualTo("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n67\n68\nFizz\nBuzz\n71\nFizz\n73\n74\nFizzBuzz\n76\n77\nFizz\n79\nBuzz\nFizz\n82\n83\nFizz\nBuzz\n86\nFizz\n88\n89\nFizzBuzz\n91\n92\nFizz\n94\nBuzz\nFizz\n97\n98\nFizz\nBuzz")
     }
 }
