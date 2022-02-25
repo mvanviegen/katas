673a422cb3d4bc18fb00cd4d5dfdeaf68866c074
mikevanviegen <mike.van.viegen@rabobank.nl>
2022-02-25 15:06:43 +0100

    initial test with implementation

--- kotlin/code-katas/fizzbuzz/src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ kotlin/code-katas/fizzbuzz/src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -1,26 +1,11 @@
 package fizzbuzz
 
 class FizzBuzz {
-    fun translate(input: List<Int>): String {
-        val result = input.map { number ->
-
-            if( number % 15 == 0 ) {
-                "FizzBuzz"
-            } else if( number % 5 == 0 ) {
-                "Buzz"
-            } else if( number % 3 == 0 ) {
-                "Fizz"
-            } else {
-                number.toString()
-            }
-
-        }.joinToString(", ")
-
-        println(result)
-        return result
+    fun parse(input: Int): String {
+        return input.toString()
     }
 }
 
 fun main() {
-    FizzBuzz().translate(listOf(1, 2, 3, 4))
+
 }
